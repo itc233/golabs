2d1fa56fface7205788a23e30d5113f6bc1c3c84
t06085 <b01901070@ntu.edu.tw>
2018-03-14 16:53:36 -0400

    first

--- src/simplepb/server.go
+++ src/simplepb/server.go
@@ -192,6 +192,8 @@ func (srv *PBServer) Start(command interface{}) (
 				/*if(count == len(prm_sv.peers)/2 +1){
 					prm_sv.commitIndex = len(prm_sv.log)
 				}*/
+			}else{
+				fmt.Printf("Fail: %d\n", i)
 			}
 			/*
 			View          int         // the primary's current view
@@ -246,22 +248,20 @@ func (srv *PBServer) Prepare(args *PrepareArgs, reply *PrepareReply) {
 	}else{
 		reply.Success = false
 		if(srv.currentView < args.View || len(srv.log) < args.Index){
-			go func(){
-				rec_arg := RecoveryArgs{
-					View: args.View, // the view that the backup would like to synchronize with
-					Server: srv.me, // the server sending the Recovery RPC (for debugging)
-				}
-				var rec_reply RecoveryReply
-				prim_id := GetPrimary(args.View, len(srv.peers))
-				srv.peers[prim_id].Call("PBServer.Recovery", &rec_arg, &rec_reply)
-				if(rec_reply.Success){
-					srv.log = rec_reply.Entries
-					srv.currentView = rec_reply.View
-					srv.commitIndex = rec_reply.PrimaryCommit
-					srv.log = append(srv.log, args.Entry)
-					reply.Success = true
-				}
-			}()
+			rec_arg := RecoveryArgs{
+				View: args.View, // the view that the backup would like to synchronize with
+				Server: srv.me, // the server sending the Recovery RPC (for debugging)
+			}
+			var rec_reply RecoveryReply
+			prim_id := GetPrimary(args.View, len(srv.peers))
+			srv.peers[prim_id].Call("PBServer.Recovery", &rec_arg, &rec_reply)
+			if(rec_reply.Success){
+				srv.log = rec_reply.Entries
+				srv.currentView = rec_reply.View
+				srv.commitIndex = rec_reply.PrimaryCommit
+				srv.log = append(srv.log, args.Entry)
+				reply.Success = true
+			}
 		}
 	}
 }
