a48265a8a1ba38b2f3fbc145e3d3b8868ad48df3
t06085 <b01901070@ntu.edu.tw>
2018-03-16 11:59:11 -0400

    first

--- src/simplepb/server.go
+++ src/simplepb/server.go
@@ -211,9 +211,9 @@ func (srv *PBServer) Start(command interface{}) (
 				if(count == len(prm_sv.peers)/2 +1){
 					prm_sv.commitIndex = prm_sv.commitIndex +1
 				} 
-			}else if(rpc_ok && reply.Success == false){
+			}/*else if(rpc_ok && reply.Success == false){
 				i = i-1
-			}
+			}*/
 			/*fmt.Printf("crtIndex %d server %d\n", prm_sv.crtIndex, i)
 			if(rpc_ok == false){
 				fmt.Printf(" fail\n")
