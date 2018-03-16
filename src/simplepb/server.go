00ebd3150bd90856854f3725fdc112b44faec7ec
t06085 <b01901070@ntu.edu.tw>
2018-03-16 02:16:48 -0400

    first

--- src/simplepb/server.go
+++ src/simplepb/server.go
@@ -257,7 +257,7 @@ func (srv *PBServer) Prepare(args *PrepareArgs, reply *PrepareReply) {
 	if(srv.me == GetPrimary(args.View, len(srv.peers))){
 		reply.Success = true
 	}else if(args.View == srv.currentView && args.Index == len(srv.log)){
-		fmt.Printf("srv.me %d arg.Index %d len(srv.log) %d\n", srv.me, args.Index, srv.log)
+		//fmt.Printf("srv.me %d arg.Index %d len(srv.log) %d\n", srv.me, args.Index, srv.log)
 		srv.log = append(srv.log, args.Entry)
 		//srv.commitIndex = srv.commitIndex+1
 		srv.commitIndex = args.PrimaryCommit
@@ -265,7 +265,7 @@ func (srv *PBServer) Prepare(args *PrepareArgs, reply *PrepareReply) {
 
 	}else if(srv.currentView < args.View){
 		reply.Success = false
-		fmt.Printf("srv.currentViewv:%d args.View:%d len(srv.log):%d args.Index:%d\n", srv.currentView, args.View, len(srv.log), args.Index)
+		//fmt.Printf("srv.currentViewv:%d args.View:%d len(srv.log):%d args.Index:%d\n", srv.currentView, args.View, len(srv.log), args.Index)
 		if(srv.currentView < args.View || len(srv.log) < args.Index){
 			fmt.Printf("Recovery\n")
 		}
@@ -285,8 +285,8 @@ func (srv *PBServer) Prepare(args *PrepareArgs, reply *PrepareReply) {
 			}
 		//}
 	}else{
-		fmt.Printf("succeed false: me %d, ", srv.me)
-		fmt.Printf("srv.currentViewv:%d args.View:%d len(srv.log):%d args.Index:%d\n", srv.currentView, args.View, len(srv.log), args.Index)
+		//fmt.Printf("succeed false: me %d, ", srv.me)
+		//fmt.Printf("srv.currentViewv:%d args.View:%d len(srv.log):%d args.Index:%d\n", srv.currentView, args.View, len(srv.log), args.Index)
 		reply.Success = false
 	}
 }
